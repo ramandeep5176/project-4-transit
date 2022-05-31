0df0f0fbaa3f1b7f70ebe8e39221445162ec804c
ramandeep5176 <ramandeepgill5176@gmail.com>
2022-05-31 15:49:36 -0400

    done

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+// import styled from "styled-components";
 import { Hero } from "../components/Index";
 const HomePage = () => {
   return (
